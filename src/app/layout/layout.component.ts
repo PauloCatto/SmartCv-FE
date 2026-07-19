79261585e70b8363a89ec969b56dd2351860dfde
Paulo Catto <paulohcatto@hotmail.com>
2026-07-19 19:19:49 -0300

    feat: implement layout structure with header and sidebar components

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header';
 import { SidebarComponent } from './sidebar/sidebar';
 import { FooterComponent } from './footer/footer';
+import { SidebarService } from '../core/services/sidebar.service';
 
 @Component({
   selector: 'app-layout',
@@ -10,4 +11,6 @@ import { FooterComponent } from './footer/footer';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  sidebarService = inject(SidebarService);
+}
