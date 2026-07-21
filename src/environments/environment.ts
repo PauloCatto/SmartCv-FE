17b26a538c497f83c1f090dd345191a7fe4f825c
Paulo Catto <paulohcatto@hotmail.com>
2026-07-21 16:22:20 -0300

    feat: initialize environment configuration with production and local API endpoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
+const isProd = window.location.hostname !== 'localhost';
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000',
-  uploadUrl: 'http://localhost:3000',
+  production: isProd,
+  apiUrl: isProd ? 'https://api-smart-cv.vercel.app' : 'http://localhost:3000',
+  uploadUrl: isProd ? 'https://api-smart-cv.vercel.app' : 'http://localhost:3000',
   geminiApiKey: '',
   googleClientId: '67534728936-13lfj27iblcfj22h9hg4koir6pcfmv09.apps.googleusercontent.com'
 };
